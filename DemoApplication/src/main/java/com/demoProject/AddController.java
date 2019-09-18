8ca4f19077034f6701f9e86afee2c1b8c9d59b3f
SHANATHRAJ <SHANATHRAJ@LAPTOP-C9T5OKDJ>
2019-09-18 07:33:55 +0530

    refs string added commit

--- DemoApplication/src/main/java/com/demoProject/AddController.java
+++ DemoApplication/src/main/java/com/demoProject/AddController.java
@@ -6,12 +6,13 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class AddController {
 	@RequestMapping("/")
 	public String indexPage()
 	{
-		
+		String a="shananth";
 	   return "apolloIndex.jsp";
 	}
 
